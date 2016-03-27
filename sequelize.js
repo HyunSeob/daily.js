d696801ebf7c695c8f42100f5c9fa75701b808e9
HyunSeob <leehs1017@naver.com>
2016-03-27 23:07:33 +0900

    Environment change

--- sequelize.js
+++ sequelize.js
@@ -5,7 +5,7 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize(
   'practice', // 데이터베이스 이름
   'root', // 유저 명
-  'vjvj7721', // 비밀번호
+  'password', // 비밀번호
   {
     'host': 'localhost', // 데이터베이스 호스트
     'dialect': 'mysql' // 사용할 데이터베이스 종류
