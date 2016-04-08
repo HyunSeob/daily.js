e610bbe451a65698317ab80cb68445bc1b1e993f
HyunSeob <leehs1017@naver.com>
2016-04-08 23:11:07 +0900

    Add Function:curry part

--- the-good-parts.js
+++ the-good-parts.js
@@ -269,3 +269,17 @@ seqer.setPrefix('Q');
 seqer.setSeq(1000);
 
 var unique = seqer.gensyn();
+
+// 14: Curry
+
+Function.method('curry', function() {
+  var slice = Array.prototype.slice,
+      args = slice.apply(arguments),
+      that = this;
+  return function() {
+    return that.apply(null, args.concat(slice.apply(arguments)));
+  };
+});
+
+var add1 = add.curry(1);
+console.log(add1(6));
