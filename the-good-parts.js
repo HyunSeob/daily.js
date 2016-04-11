ae9a21bfe41bf107050527735bcee383b0f9386c
HyunSeob <leehs1017@naver.com>
2016-04-11 22:53:44 +0900

    Add Function:memoization part

--- the-good-parts.js
+++ the-good-parts.js
@@ -283,3 +283,45 @@ Function.method('curry', function() {
 
 var add1 = add.curry(1);
 console.log(add1(6));
+
+// 15: Memoization
+
+var fibonacci = function(n) {
+  return n < 2 ? n : fibonacci(n - 1) + fibonacci(n - 2);
+};
+
+for (var i = 0; i <= 10; i += 1) {
+  console.log('// ' + i + ': ' + fibonacci(i));
+}
+
+var fibonacci = function() {
+  var memo = [0, 1];
+  var fib = function(n) {
+    var result = memo[n];
+    if (typeof result !== 'number') {
+      result = fib(n - 1) + fib(n - 2);
+      memo[n] = result;
+    }
+  };
+  return fib;
+}();
+
+var memoizer = function(memo, fundamental) {
+  var shell = function(n) {
+    var result = memo[n];
+    if (typeof result !== 'number') {
+      result = fundamental(shell, n);
+      memo[n] = result;
+    }
+    return result;
+  };
+  return shell;
+};
+
+var fibonacci = memoizer([0, 1], function(shell, n) {
+  return shell(n - 1) + shell(n - 2);
+});
+
+var factorial = memoizer([1, 1], function(shell, n) {
+  return n * shell(n - 1);
+});
