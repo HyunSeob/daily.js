13a474a0d9f3c099a7b904e7cdcb0bcde2e3bece
HyunSeob <leehs1017@naver.com>
2016-04-23 14:06:07 +0900

    Add array part

---
+++ the-good-parts/06-Array/01-literal.js
@@ -0,0 +1,11 @@
+var empty = [];
+var numbers = [
+  'zero', 'one', 'two', 'three', 'four',
+  'five', 'six', 'seven', 'eight','nine'
+];
+
+console.log(empty[1]);
+console.log(numbers[1]);
+
+console.log(empty.length);
+console.log(numbers.length);
